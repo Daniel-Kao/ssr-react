125f3f89e3ecb9cac46c5594eadb25894f3a125c
kaojixiang <396007476@qq.com>
2019-09-01 15:07:06 +0800

    add react render support

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,21 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
 import Home from '../containers/Home';
+const app = express();
 
-app.get('/', (req, res) => res.send('hello worldasd'));
+const content = renderToString(<Home />);
+
+app.get('/', (req, res) => {
+  res.send(`
+  <html>
+    <head>
+    </head>
+    <body>
+    ${content}
+    </body>
+  </html>
+  `);
+});
 
 app.listen(3000, () => console.log('server started on port 3000'));
